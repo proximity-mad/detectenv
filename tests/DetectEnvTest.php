adc0de79c248f9c968a4d69a2f9516bb8a450253
n.vanderburg <nvanderburg@proximity.es>
2016-11-07 12:42:00 +0100

    Added unit tests

---
+++ tests/DetectEnvTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace proximity\detectenv;
+
+
+use PHPUnit\Framework\TestCase;
+
+class DetectEnvTest extends TestCase
+{
+    public function testDetectEnvEmpty()
+    {
+        $envs = [];
+        $this->assertEquals(DetectEnv::get($envs), null);
+    }
+
+    public function testDetectEnv()
+    {
+        $envs = ['localhost' => 'dev'];
+        $this->assertNotEquals(DetectEnv::get($envs), 'dev');
+    }
+
+    public function testDetectEnvFake()
+    {
+        $_SERVER['SERVER_NAME'] = 'localhost';
+        $envs = ['localhost' => 'dev'];
+        $this->assertEquals(DetectEnv::get($envs), 'dev');
+    }
+
+    public function testDetectEnvProdFake()
+    {
+        $_SERVER['SERVER_NAME'] = 'mydomain.dev';
+        $envs = ['localhost' => 'dev', 'mydomain.dev' => 'prod'];
+        $this->assertEquals(DetectEnv::get($envs), 'prod');
+    }
+
+    public function testDetectEnvDefault()
+    {
+        $envs = ['localhost' => 'dev'];
+        $this->assertEquals(DetectEnv::get($envs, 'dev'), 'dev');
+    }
+}
